a1f66d1d48cbaec6e39780e7e4c95e27b1682a62
Silas <Silas@Silass-Mac-mini.local>
2019-12-17 19:16:56 +0800

    Mac Editor 下使用原来的Unity Plugin

--- Runtime/PersistentConnection/KCPLib.cs
+++ Runtime/PersistentConnection/KCPLib.cs
@@ -68,7 +68,7 @@ namespace Capstones.Net
         [DllImport(LIB_PATH, CallingConvention = CallingConvention.Cdecl)]
         public static extern void kcp_memmove(IntPtr dst, IntPtr src, int cnt);
 
-#if !UNITY_ENGINE && !UNITY_5_3_OR_NEWER || UNITY_EDITOR_OSX
+#if !UNITY_ENGINE && !UNITY_5_3_OR_NEWER
         static KCPLib()
         {
             UnityEngineEx.PluginManager.LoadLib(LIB_PATH);
