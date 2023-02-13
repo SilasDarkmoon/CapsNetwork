aaa94db317d32d332be14281b2b2078f5c0a527e
Silas <hdss3609@gmail.com>
2023-02-13 20:54:31 +0800

    将PVP消息的最大读取长度加大到8MB

--- Runtime/PersistentConnection/NetConsts.cs
+++ Runtime/PersistentConnection/NetConsts.cs
@@ -13,7 +13,7 @@ namespace Capstones.Net
     public static class CONST
     {
         public const int MTU = 1400; // KCP default mtu.
-        public const int MAX_MESSAGE_LENGTH = 1024 * 1024; // The longest message is limited to 1MB, that's big enough. If we got a message larger than that, we should treat it as an error.
+        public const int MAX_MESSAGE_LENGTH = 8 * 1024 * 1024; // The longest message is limited to 8MB, that's big enough. If we got a message larger than that, we should treat it as an error.
         public const int MAX_SERVER_PENDING_CONNECTIONS = 100;
         public const int DEFAULT_IDLE_TIMEOUT = 15000;
         public const int MAX_QUEUED_MESSAGE = 1000;
